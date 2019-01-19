dc4536cf1ee201bf4f7537cb8a0a9a57c45fddec
arvind2208 <aravinthan.anbuvel@gmail.com>
2019-01-20 09:15:06 +1100

    Initialize with base scaffold, decide application work flow

---
+++ dotnet-code-challenge/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dotnet_code_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For all files, load them into a common domain model/DataSet
+            //Adapter to load the model from different providers
+            //A strategy to decide which adapter to use for each of the backend services
+
+            //Get results by invoking GetHorsesByPrice Service
+
+            //Print the results
+            Console.WriteLine("Results printed successfully");
+        }
+    }
+}

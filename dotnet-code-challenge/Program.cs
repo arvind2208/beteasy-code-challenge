094c0fd69a882e7c242965f41de1d1d2156e597f
arvind2208 <aravinthan.anbuvel@gmail.com>
2019-01-20 11:29:29 +1100

    Cleanup main, setup dependency injection, make feedsData path configurable

--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -2,9 +2,10 @@
 using dotnet_code_challenge.Helpers;
 using dotnet_code_challenge.Models;
 using dotnet_code_challenge.Renderers;
-using dotnet_code_challenge.Repositories;
 using dotnet_code_challenge.Services;
-using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 using System.IO;
 
 namespace dotnet_code_challenge
@@ -13,14 +14,23 @@ namespace dotnet_code_challenge
     {
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            var config = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            Startup.ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            DataSet dataSet = (DataSet)serviceProvider.GetService(typeof(DataSet));
+            string basePath = config["BasePath"];
+            var fileSystem = serviceProvider.GetService<IFileSystem>();
+
             //For all files, load them into a common domain model/DataSet
             //Adapter to load the model from different providers
             //A strategy to decide which adapter to use for each of the backend services
-            //TODO: make basePath configurable
-            string basePath = @"C:\interview\beteasy-code-challenge\dotnet-code-challenge\FeedData\";
-            var dataSet = new DataSet();
-            var fileSystem = new FileSystem();
-
             if (Directory.Exists(basePath))
             {
                 foreach (string file in Directory.GetFiles(basePath))
@@ -31,15 +41,11 @@ namespace dotnet_code_challenge
                 }
             }
 
-            //Initialize repositories //TODO:
-            var horseRepository = new HorseRepository(dataSet);
             //Get results by invoking GetHorsesByPrice Service
-            var horses = (new GetHorsesByPriceService(horseRepository)).Invoke();
+            var horses = serviceProvider.GetService<IService<IEnumerable<Horse>>>().Invoke();
+            
             //Print the results
-
-            (new HorsesByPriceRenderer(new ConsoleWriter())).Render(horses);
-
-            Console.WriteLine("Results printed successfully");
+            serviceProvider.GetService<IRenderer<IEnumerable<Horse>>>().Render(horses);
         }
     }
 }
---
+++ dotnet-code-challenge/Startup.cs
@@ -0,0 +1,23 @@
+﻿using dotnet_code_challenge.Helpers;
+using dotnet_code_challenge.Models;
+using dotnet_code_challenge.Renderers;
+using dotnet_code_challenge.Repositories;
+using dotnet_code_challenge.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+
+namespace dotnet_code_challenge
+{
+    public class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(new DataSet());
+            services.AddTransient<IFileSystem, FileSystem>();
+            services.AddTransient<IRepository<Horse>, HorseRepository>();
+            services.AddTransient<IService<IEnumerable<Horse>>, GetHorsesByPriceService>();
+            services.AddTransient<IRenderer<IEnumerable<Horse>>, HorsesByPriceRenderer>();
+            services.AddTransient<IWriter, ConsoleWriter>();
+        }
+    }
+}
